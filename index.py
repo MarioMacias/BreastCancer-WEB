7a8ba9793452f240d852fba963b0ea0b042bee46
MARIO ALBERTO MACIAS ESCOBEDO <mariomacias77@hotmail.com>
2021-03-24 16:41:48 -0600

    Macros

--- index.py
+++ index.py
@@ -1,26 +1,31 @@
-from flask import Flask, render_template,request
-
+from flask import Flask, render_template, request
+from forms import *
 app = Flask(__name__)
+
+app.secret_key = "@proy321"
+data = []
+famele = True
 
 #Este es para el index
 @app.route('/',methods=['GET','POST']) 
-def formulario():
-    if request.method == 'POST':
-        peso = request.form.get("peso")
-        estatura = request.form.get("estatura")
-        fuma = request.form.get("fuma")
-        toma = request.form.get("toma")
-        return render_template("resultados.html",peso=peso,estatura=estatura,toma=toma,fuma=fuma)
-        
-    return render_template('formulario.html')
+def index():
+     return render_template('prueba.html')
 
-@app.route('/form1',methods=["POST"])
-def form1():
-    peso = request.form.get("peso")
-    estatura = request.form.get("estatura")
-    fuma = request.form.get("fuma")
-    toma = request.form.get("toma")
-    return render_template("resultados.html",peso=peso,estatura=estatura,toma=toma,fuma=fuma)
+@app.route('/formulario/<panel>', methods=['GET', 'POST'])
+def formulario(panel=1):
+    form = Bloque1Form()
+    return render_template('formulario.html',panel=panel, form = form)
+    # if request.method == 'POST':
+    #     peso = request.form.get("peso")
+    #     estatura = request.form.get("estatura")
+    #     fuma = request.form.get("fuma")
+    #     toma = request.form.get("toma")
+    #     list = [["peso",peso],["estatura",estatura], ["fuma",fuma], ["toma",toma]]
+    #     data.append(list)
+
+# @app.route('/agregarDatos')
+# def agregarDatos():
+#    return render_template('formulario.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
